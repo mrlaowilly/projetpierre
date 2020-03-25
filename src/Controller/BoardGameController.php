63b3ea0e98e18276e0d1497c520a14cc50981792
mrlaowilly <willy.meksavanh@hotmail.fr>
2020-03-25 11:39:36 +0100

    formulaire suite

--- src/Controller/BoardGameController.php
+++ src/Controller/BoardGameController.php
@@ -6,8 +6,10 @@ namespace App\Controller;
 use App\Entity\BoardGame;
 use App\Repository\BoardGameRepository;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -42,9 +44,9 @@ class BoardGameController extends AbstractController
     }
 
     /**
-     * @Route("/new")
+     * @Route("/new", methods={"GET", "POST"})
      */
-    public function new()
+    public function new(Request $request, EntityManagerInterface $manager)
     {
         $game = new BoardGame();
 
@@ -65,8 +67,61 @@ class BoardGameController extends AbstractController
             ])
             ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($game);
+            $manager->flush();
+
+            $this->addFlash('success', 'Nouveau jeu crée');
+            return $this->redirectToRoute('app_boardgame_show', [
+                'id' => $game->getId(),
+            ]);
+        }
+
         return $this->render('board_game/new.html.twig', [
             'new_form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", methods={"GET", "PUT"})
+     */
+    public function edit(BoardGame $game, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createFormBuilder($game, [
+            'method' => 'PUT',
+        ])
+            ->add('name', null, [
+                'label' => 'nom',
+            ])
+            ->add('description', null, [
+                'label' => 'Description',
+            ])
+            ->add('releasedAt', DateType::class, [
+                'html5' => true,
+                'widget' => 'single_text',
+                'label' => 'Date de sortie',
+            ])
+            ->add('ageGroup', null, [
+                'label' => 'A partir de',
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            $this->addFlash('success', 'mis a jour');
+            return $this->redirectToRoute('app_boardgame_show', [
+                'id' => $game->getId(),
+            ]);
+        }
+
+        return $this->render('board_game/edit.html.twig', [
+            'game' => $game,
+            'edit_form' => $form->createView(),
         ]);
     }
 }
